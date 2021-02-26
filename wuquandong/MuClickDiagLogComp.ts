0b7648d1abff02dce7dcad4585dff2834711239e
wuquandong <wuquandong@100daishu.com>
2021-02-26 09:57:27 +0800

    多个对话框添加是否替换显示的逻辑、音频文件放到对话框的层级

--- wuquandong/MuClickDiagLogComp.ts
+++ wuquandong/MuClickDiagLogComp.ts
@@ -9,17 +9,36 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class MuClickDiagLogComp extends DiagLogComp {
 
-    btnClick(sender: cc.Node) {
-        sender.children.forEach((child, index) => {
-            child.opacity = index <= this._curIndex ? 255 : 0;
-            if (index == this._curIndex) {
-                if (child.getComponentInChildren(cc.AudioSource)) {
-                    child.getComponentInChildren(cc.AudioSource).play()
-                }
-            }
+    @property({
+        tooltip: ''
+    })
+    isCover: boolean = true;
 
-        })
+    btnClick(sender: cc.Node) {
         this._curIndex++;
+        sender.children.forEach(child => {
+            child.active = false;
+        })
+        if (this.isCover) {
+            if (sender.children[this._curIndex - 1]) {
+                sender.children[this._curIndex - 1].opacity = 255;
+                sender.children[this._curIndex - 1].active = true;
+            }
+        } else {
+            sender.children.forEach((child,index) => {
+                child.active = index < this._curIndex;
+                child.opacity = index < this._curIndex ? 255 : 0;
+            })
+        }
+
+        if (sender.children[this._curIndex - 1].getComponentInChildren(cc.AudioSource)) {
+            sender.children[this._curIndex - 1].getComponentInChildren(cc.AudioSource).play();
+            return
+        }
+        if (sender.children[this._curIndex - 1].getComponent(cc.AudioSource)) {
+            sender.children[this._curIndex - 1].getComponent(cc.AudioSource).play();
+            return
+        }
     }
 
     protected _curIndex: number = 0;
